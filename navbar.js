5509c5ff8bba36f8e35cad6324a6268faf2b67a6
guddu tiwari <gt29015@gmail.com>
2022-08-26 18:15:58 +0530

    day 4 task completed

---
+++ navbar.js
@@ -0,0 +1,17 @@
+let dropbtn = document.querySelector("#dropbtn");
+dropbtn.addEventListener("click", () => {
+    document.getElementById("myDropdown").classList.toggle("show");
+})
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
